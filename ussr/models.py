01ff76f3e5f4cdfbd8f051925a0d5eefcb4880c1
rskvprd <vyackov2001@mail.ru>
2022-05-11 21:55:26 +0300

    Доделал модели(почти(ну вообще не доделал))

--- ussr/models.py
+++ ussr/models.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from django.db import models
 from django.contrib.auth.models import User as BaseUser
 '''Чекайте модель базы данных, которую делал Артём (важная заметочка)'''
@@ -6,7 +7,7 @@ class User(BaseUser):
     level = models.CharField() # Чекайте систему рангов
 
 
-class Ingredient:
+class Ingredient(models.Model):
     name = models.CharField(max_length=255)
     calories = models.IntegerField(blank=True)
     proteins = models.IntegerField(blank=True)
@@ -19,7 +20,7 @@ class Ingredient:
         return self.name + self.type if self.type is not None else self.name
 
 
-class Recipe:
+class Recipe(models.Model):
     name = models.CharField('Название рецепта', max_length=255)
     text = models.TextField('Текст рецепта', max_length=10000)
     cooking_time = models.TimeField('Время готовки', default='00:05', blank=True)
@@ -32,6 +33,24 @@ class Recipe:
     last_edited_datetime = models.DateTimeField('Время последнего изменения', auto_now=True)
 
 
-class Review:
+class Review(models.Model):
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    #TODO: Доделать модели
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL)
+    text = models.TextField('Текст отзыва')
+    create_datetime = models.DateTimeField('Время создания', auto_now_add=True)
+    # rate = models.ManyToManyField(User)
+    # TODO: Узнать как тут нормально реализовать многие ко многим
+
+
+class Bookmarks(models.Model):
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_datetime = models.DateTimeField('Дата создания', auto_now_add=True)
+
+
+class Composition(models.Model):
+    recipe_id = models.ForeignKey(Recipe)
+    ingredient = models.ForeignKey(Ingredient)
+    volume = models.CharField('Количество/Вес', max_length=255, blank=True)
+    is_required = models.BooleanField('Обязательный')
+    
