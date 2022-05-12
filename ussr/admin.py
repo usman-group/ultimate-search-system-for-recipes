ef8a5a7bac451f9f50651dcc805f4fcfe409af8f
rskvprd <vyackov2001@mail.ru>
2022-05-12 18:16:57 +0300

    Модели доделал(ну навернео()

--- ussr/admin.py
+++ ussr/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Ingredient
+from .models import *
 
-# class IngredientAdmin(admin.ModelAdmin):
-#     fields = 
-
-
+admin.site.register(Composition)
+admin.site.register(Recipe)
+admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(ReviewRate)
+admin.site.register(RecipeRate)
+admin.site.register(Bookmarks)
 admin.site.register(Ingredient)
--- ussr/models.py
+++ ussr/models.py
@@ -1,8 +1,11 @@
-from statistics import mode
-from tabnanny import verbose
 from django.db import models
+from django.core import validators
 from django.contrib.auth.models import User as BaseUser
-'''Чекайте модель базы данных, которую делал Артём (важная заметочка)'''
+
+'''
+Чекайте модель базы данных, которую делал Артём (важная заметочка)
+'''
+
 
 class User(BaseUser):
     level = models.CharField('Уровень пользователя', max_length=255) # Чекайте систему рангов
@@ -10,11 +13,11 @@ class User(BaseUser):
 
 class Ingredient(models.Model):
     name = models.CharField('название', max_length=255)
-    calories = models.FloatField('калорий на 100г', blank=True)
+    calories = models.FloatField('килокалорий на 100г', blank=True)
     proteins = models.FloatField('белков на 100г', blank=True)
     fats = models.FloatField('жиров на 100г', blank=True)
     carbohydrates = models.FloatField('углеводов на 100г', blank=True)
-    type = models.CharField('тип продукта \n(например, для помидора: "Черри")', max_length=255, blank=True)
+    type = models.CharField('тип продукта (например, для помидора: "Черри")', max_length=255, blank=True)
 
     def __str__(self) -> str:
         return self.get_full_name()
@@ -26,6 +29,7 @@ class Ingredient(models.Model):
         verbose_name = 'ингредиент'
         verbose_name_plural = 'ингредиенты'
 
+
 class Recipe(models.Model):
     name = models.CharField('название рецепта', max_length=255)
     text = models.TextField('текст рецепта', max_length=10000)
@@ -34,9 +38,14 @@ class Recipe(models.Model):
     video = models.FileField('видео рецепта', upload_to='recipes/videos', blank=True)
     country = models.CharField('страна происхождения рецепта', max_length=255, blank=True)
     type = models.CharField('тип блюда', max_length=255) # Such as Борщи, Амогусы
-    cooking_type = models.CharField('тип приготовления', max_length=255) # Such as Жарка, Варка
+    cooking_type = models.CharField('тип приготовления', max_length=255) # Such as Жарка, Варка TODO: выяснить: нужно ли это вообще
     create_datetime = models.DateTimeField('время создания', auto_now_add=True)
     last_edited_datetime = models.DateTimeField('время последнего изменения', auto_now=True)
+    average_rate = models.PositiveSmallIntegerField('средняя оценка', validators=[validators.MaxValueValidator(10)], null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'рецептик'
+        verbose_name_plural = 'рецептики'
 
 
 class Review(models.Model):
@@ -44,19 +53,59 @@ class Review(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='пользователь, оставивший отзыв')
     text = models.TextField('текст отзыва')
     create_datetime = models.DateTimeField('время создания', auto_now_add=True)
-    # rate = models.ManyToManyField(User)
-    # TODO: Узнать как тут нормально реализовать многие ко многим и сделать
+    average_rate = models.PositiveIntegerField('средняя оценка отзыва', validators=[validators.MaxValueValidator(10)], null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'отзывы'
+
+
+class ReviewRate(models.Model):
+    review_id = models.ForeignKey(Review, on_delete=models.CASCADE, verbose_name='отзыв, на который поставлена оценка')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь, поставивший оценку')
+    average_rate = models.PositiveSmallIntegerField('оценка отзыва от 1 до 10', validators=[validators.MaxValueValidator(10)], default=0)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['review_id', 'user_id'], name='unique_reviewid-userid_reviewrate')
+        ]
+        verbose_name = 'оценка отзыва'
+        verbose_name_plural = 'оценки отзывов'
+
+
+class RecipeRate(models.Model):
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE, verbose_name='рецепт, на который поставлена оценка')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь, поставивший оценку')
+    rate = models.PositiveSmallIntegerField('оценка рецепта от 1 до 10', validators=[validators.MaxValueValidator(10)])
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['recipe_id', 'user_id'], name='unique_recipeid-userid_reciperate')
+        ]
+        verbose_name = 'оценка рецепта'
+        verbose_name_plural = 'оценки рецептов'
 
 
 class Bookmarks(models.Model):
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     create_datetime = models.DateTimeField('дата создания', auto_now_add=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['recipe_id', 'user_id'], name='unique_recipeid-userid_bookmarks')
+        ]
+        verbose_name = 'заметка'
+        verbose_name_plural = 'заметки'
 
 
 class Composition(models.Model):
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.SET_NULL, blank=True, null=True)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, blank=True, null=True)
     volume = models.CharField('количество / вес', max_length=255, blank=True)
     is_required = models.BooleanField('обязательный ингредиент', default=False)
     type_is_required = models.BooleanField('обязательный тип ингредиента', default=False)
+
+    class Meta:
+        verbose_name = 'состав'
+        verbose_name_plural = 'составы'
