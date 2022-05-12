e9deb31ef22dd7951ccef560cf3fd8b0fbd58752
rskvprd <vyackov2001@mail.ru>
2022-05-12 22:32:33 +0300

    добавил сериализаторы

--- ussr/serializers.py
+++ ussr/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-from ussr.models import Recipe
+from ussr.models import *
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -21,3 +21,39 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = '__all__'
         read_only_fields = ['average_rate']
+
+
+class CompositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Composition
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ReviewRateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewRate
+        fields = '__all__'
+
+
+class RecipeRateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeRate
+        fields = '__all__'
+
+
+class BookmarksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookmarks
+        fields = '__all__'
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
